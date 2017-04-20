f43d6140b1a01863cfb5e429354acfaf88d59148
tivapan <tivapan09@gmail.com>
2017-04-20 20:42:34 +0700

    Add files via upload

---
+++ main.php
@@ -0,0 +1,74 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delicious Bootstrap Theme Demo</title>
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i' type='text/css' media='all' />
+    <link rel="stylesheet" href="https://bootstrapmade.com/demo/assets/css/normalize.css">
+    <link rel="stylesheet" href="https://bootstrapmade.com/demo/assets/css/fontello.css">
+    <link rel="stylesheet" href="https://bootstrapmade.com/demo/assets/css/style.css">
+</head>
+
+<body>
+    <header id="header">
+        
+        <div class="logo">
+            <a href="https://bootstrapmade.com/"><img src="https://bootstrapmade.com/wp-content/themes/bmade/assets/img/logo.png" title="Back to BootstrapThemes.io Homepage" /></a>
+        </div>
+        <!--
+        <div class="info">
+            <a title="You are currently previewing the Delicious Theme" href="https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/">Delicious Theme</a>
+        </div>
+        -->
+        <div class="preview-devices">
+            <ul>
+                <li class="preview-test preview-devices-active" id="preview-test-desktop" title="Preview the  Delicious Theme in Desktop Mode"><a href=""><i class="icon-desktop"></i></a></li>
+                 <!--<li class="preview-test" id="preview-test-tablet" title="Preview the  Delicious Theme in Tablet Mode"><a href=""><i class="icon-tablet"></i></a></li>-->
+                <li class="preview-test" id="preview-test-mobile" title="Preview the  Delicious Theme in Mobile Mode"><a href=""><i class="icon-mobile"></i></a></li>
+            </ul>
+        </div>
+        
+        <div class="navigate"> 
+            <ul>
+                <!--<li class="navigate-like"><iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Ffacebook.com%2FBootstrapMade&width=63&layout=button&action=like&size=large&show_faces=false&share=false&height=65&appId=163862247017518" width="63" height="28" style="border:none;overflow:hidden;float: left; padding-top: 7px;" scrolling="no" frameborder="0" allowTransparency="true"></iframe></li> -->
+                <li><a href="https://bootstrapmade.com/demo/themes/Delicious/" target="_top" title="Hide the demo bar"><i class="icon-exit"></i></a></li> 
+                <li><a href="https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/" title="Delicious Theme Home Page"><i class="icon-home"></i></a></li>
+                <li><a class="download" href="https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/?download=delicious.zip" title="Download the Delicious Theme"><i class="icon-download"></i> <span>Free Download</span></a></li>
+                                    <li><a href="https://bootstrapmade.com/demo/Mentor/" title="Previous Theme: Mentor" target="_top"><i class="icon-left-arrow"></i></a></li>    
+                                
+                                    <li><a href="https://bootstrapmade.com/demo/MyBiz/" title="Next Theme: MyBiz" target="_top"><i class="icon-right-arrow"></i></a></li>    
+                            </ul>
+        </div>
+    
+    </header>
+
+    <div id="preview">
+        <iframe id="preview-frame" class="preview-desktop" src="https://bootstrapmade.com/demo/themes/Delicious/" frameborder="0"></iframe>
+    </div>
+    <!--
+    <div class="social-share" title="Like &amp; Share the Delicious Theme">
+        Like &amp; Share: &nbsp;
+        <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Ffacebook.com%2FBootstrapMade&width=63&layout=button&action=like&size=large&show_faces=false&share=false&height=65&appId=163862247017518" width="63" height="28" style="border:none;overflow:hidden;margin-bottom: -9px;" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+        <a title="Share on Twitter" href="https://twitter.com/intent/tweet?url=https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/&text=Delicious+Bootstrap+Theme&via=BootstrapMade" target="_blank"><i class="icon-twitter"></i></a>
+        <a title="Share on Facebook" href="https://www.facebook.com/sharer.php?u=https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/"  target="_blank"><i class="icon-facebook"></i></a> 
+        <a title="Share on Pinterest" href="https://pinterest.com/pin/create/button/?url=https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/&media=https://bootstrapmade.com/wp-content/themefiles/Delicious/800.png"  target="_blank"><i class="icon-pinterest-circled"></i></a> 
+        <a title="Share on Google Plus" href="https://plus.google.com/share?url=https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/" target="_blank"><i class="icon-gplus"></i></a> 
+        <i class="icon-cancel-circled" title="Close the sharing bar"></i>
+    </div>
+    -->
+    <script src="https://bootstrapmade.com/demo/assets/js/jquery-3.1.0.min.js"></script>
+    <script src="https://bootstrapmade.com/demo/assets/js/custom.js"></script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-55234356-4', 'auto');
+        ga('send', 'pageview');
+    </script>
+</body>
+</html>
